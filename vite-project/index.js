9c15f87bdcede5a05c4f307425d24f3f7714ef3a
oraclelens <sophiar10200@gmail.com>
2023-01-13 13:15:43 -0500

    ok so

--- vite-project/index.js
+++ vite-project/index.js
@@ -4,15 +4,26 @@ const DOMSelectors = {
   disneybutton: document.getElementById("getMouseButton"),
   film: document.getElementById("film"),
   searchInput: document.getElementById("searchInput"),
+  display: document.getElementById("display"),
 };
 
-const URL = "https://api.disneyapi.dev/characters";
+const URL = "https://api.disneyapi.dev/characters?page=5";
 
 document.addEventListener("DOMContentLoaded", findFilm);
 async function getTheMouse(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
+    data.data
+      .filter((el) => el.name.includes("Mulan"))
+      .forEach((el) => {
+        DOMSelectors.display.insertAdjacentHTML(
+          "beforeend",
+          `<div>
+        <img class="characterimage" src="${el.imageUrl}"/>
+        </div>`
+        );
+      });
     console.log(data.data);
     console.log(response);
     return data;
