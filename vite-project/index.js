c2606d9e62d3fbba39b757baf78d062041a8335f
oraclelens <sophiar10200@gmail.com>
2023-01-18 13:14:43 -0500

    ok image pls show up

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,34 +1,42 @@
 import "./styles/style.css";
 
 const DOMSelectors = {
-  disneybutton: document.getElementById("getMouseButton"),
-  film: document.getElementById("film"),
-  searchInput: document.getElementById("searchInput"),
+  amiibobutton: document.getElementById("getAmiiboButton"),
+  amiibo: document.getElementById("amiibo"),
+  input: document.getElementById("input"),
   display: document.getElementById("display"),
 };
 
-const URL = "https://www.amiiboapi.com/api/gameseries/";
+const URL = "https://www.amiiboapi.com/api/amiibo/";
 
-document.addEventListener("DOMContentLoaded", findFilm);
-async function getTheMouse(URL) {
+document.addEventListener("DOMContentLoaded", findAmiibo);
+async function getAmiibo(URL) {
   try {
     const response = await fetch(URL);
-    const data = await response.json();
-    data.data
-      .filter((el) => el.name.includes("Animal Crossing"))
-      .forEach((el) => {
-        DOMSelectors.display.insertAdjacentHTML(
-          "beforeend",
-          `<div>
-        <img class="characterimage" src="${el.imageUrl}"/>
-        </div>`
-        );
-      });
-    console.log(data.data);
-    console.log(response);
-    return data;
+    if (response.status < 299) {
+      console.log(response.status);
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+      if (data.amiibo) {
+        DOMSelectors.display.innerHTML = `${amiibo.image} <br><br> ${aiimbo.name}`;
+      }
+    }
   } catch (error) {
     console.log(error);
   }
 }
-getTheMouse(URL);
+
+document
+  .getElementById("getAmiiboButton")
+  .addEventListener("click", function () {
+    let addURL = URL + "?name=" + DOMSelectors.input.value;
+    console.log(input.value);
+    console.log(addURL);
+    DOMSelectors.input.value = "";
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  getAmiibo(URL);
+});
