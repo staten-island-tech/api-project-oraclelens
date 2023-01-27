c2719646fef3e4230571d35f23c583fc9b3ce284
oraclelens <sophiar10200@gmail.com>
2023-01-27 17:12:37 -0500

    yay

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,59 +1,180 @@
 import "./styles/style.css";
 
 const DOMSelectors = {
-  button: document.querySelector("getAmiiboButton"),
-  amiibo: document.getElementById("amiibo"),
-  input: document.getElementById("input"),
   display: document.getElementById("display"),
-  container: document.getElementById("container"),
+  getAmiiboButton: document.getElementById("btn"),
+  AmiiboFun: document.getElementById("findAmiibo"),
 };
 
 const URL = "https://www.amiiboapi.com/api/amiibo";
 
-document.addEventListener("DOMContentLoaded", findAmiibo);
-async function getAmiibo(URL) {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data);
-    let input = DOMSelectors.input.value;
-    data.amiibo
-      .filter((el) => el.name == `${input}`)
-      .forEach((el) => {
-        console.log(el.name);
-        DOMSelectors.container.insertAdjacentHTML(
-          "beforeend",
-          `
-              <div class="inside">
-              <h2>${el.name}</h2>
-              <img class="abby" src="${el.image}" alt="add image">        
-              </div>`
-        );
-      });
-  } catch (error) {
-    console.log(error);
+async function getAmiibo() {
+  DOMSelectors.display.innerHTML = "";
+
+  if (DOMSelectors.AmiiboFun.value === "Mario") {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      data.amiibo
+        .filter((el) => el.name === "Mario")
+        .filter((el) => el.tail === "00340102")
+        .forEach((el) => {
+          DOMSelectors.display.insertAdjacentHTML(
+            "beforeend",
+            `<div>
+              <img class="height" src="${el.image}" alt="amiibo figure of videogame character mario">    
+            </div>`
+          );
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.log("error");
+  }
+
+  if (DOMSelectors.AmiiboFun.value === "Luigi") {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      data.amiibo
+        .filter((el) => el.name === "Luigi")
+        .filter((el) => el.tail === "00350102")
+        .forEach((el) => {
+          DOMSelectors.display.insertAdjacentHTML(
+            "beforeend",
+            `<div>
+              <img class="height" src="${el.image}" alt="amiibo figure of videogame character luigi">    
+            </div>`
+          );
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.log("error");
+  }
+
+  if (DOMSelectors.AmiiboFun.value === "Peach") {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      data.amiibo
+        .filter((el) => el.name === "Peach")
+        .filter((el) => el.tail === "00360102")
+        .forEach((el) => {
+          DOMSelectors.display.insertAdjacentHTML(
+            "beforeend",
+            `<div>
+              <img class="height" src="${el.image}" alt="amiibo figure of videogame character princess peach">    
+            </div>`
+          );
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.log("error");
+  }
+
+  if (DOMSelectors.AmiiboFun.value === "Yoshi") {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      data.amiibo
+        .filter((el) => el.name === "Yoshi")
+        .filter((el) => el.tail === "00370102")
+        .forEach((el) => {
+          DOMSelectors.display.insertAdjacentHTML(
+            "beforeend",
+            `<div>
+              <img class="height" src="${el.image}" alt="amiibo figure of videogame character yoshi">    
+            </div>`
+          );
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.log("error");
+  }
+
+  if (DOMSelectors.AmiiboFun.value === "Rosalina") {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      data.amiibo
+        .filter((el) => el.name === "Rosalina")
+        .filter((el) => el.tail === "02620102")
+        .forEach((el) => {
+          DOMSelectors.display.insertAdjacentHTML(
+            "beforeend",
+            `<div>
+              <img class="height" src="${el.image}" alt="amiibo figure of videogame character rosalina">    
+            </div>`
+          );
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.log("error");
+  }
+
+  if (DOMSelectors.AmiiboFun.value === "Bowser") {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      data.amiibo
+        .filter((el) => el.name === "Bowser")
+        .filter((el) => el.tail === "00390102")
+        .forEach((el) => {
+          DOMSelectors.display.insertAdjacentHTML(
+            "beforeend",
+            `<div>
+              <img class="height" src="${el.image}" alt="amiibo figure of videogame character bowser">    
+            </div>`
+          );
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.log("error");
+  }
+
+  if (DOMSelectors.AmiiboFun.value === "Bayonetta") {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      data.amiibo
+        .filter((el) => el.name === "Bayonetta - Player 2")
+        .filter((el) => el.tail === "03640002")
+        .forEach((el) => {
+          DOMSelectors.display.insertAdjacentHTML(
+            "beforeend",
+            `<div>
+              <img class="height" src="${el.image}" alt="amiibo figure of videogame character bayonetta">    
+            </div>`
+          );
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.log("error");
   }
 }
 
-getAmiibo(URL);
+function getImage() {
+  getAmiibo();
+}
 
-DOMSelectors.button.addEventListener("click", function () {
-  getAmiibo(URL);
-});
-/* document
-  .getElementById("getAmiiboButton")
-  .addEventListener("click", function () {
-    let addURL = URL + "?name=" + DOMSelectors.input.value;
-    console.log(input.value);
-    console.log(addURL);
-    DOMSelectors.input.value = "";
-  });
-
-document.addEventListener("DOMContentLoaded", function () {
-  getAmiibo(URL);
-});
-
-DOMSelectors.amiibobutton.addEventListener("click", function () {
-  getAmiibo(URL);
-});
- */
+DOMSelectors.getAmiiboButton.addEventListener("click", getImage);
